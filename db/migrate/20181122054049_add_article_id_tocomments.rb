dbcd47bdcc5551654aec557e88f41119de79148b
kei-iwai-koji <iw.kj.16@gmail.com>
2018-11-22 14:42:27 +0900

    Add column

---
+++ db/migrate/20181122054049_add_article_id_tocomments.rb
@@ -0,0 +1,4 @@
+class AddArticleIdTocomments < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_003917) do
+ActiveRecord::Schema.define(version: 2018_11_22_054049) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
